67496508d519421ad233f616928e682cb2220365
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-07 23:33:21 -0400

    Update category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,7 +6,15 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  try{
+    const cateData = await Category.findAll({
+     include: [{model:Product}],
+    }) 
+    res.status(200).json(cateData);
+   } catch (err) {
+     res.status(500).json(err);
+  }
+ });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
