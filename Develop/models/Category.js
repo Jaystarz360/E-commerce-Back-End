fb63f07cd587a49151f371c75b93fbc979c4736c
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-08 02:20:06 -0400

    Update Cat, index and Product

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,30 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Category extends Model {}
 
 Category.init(
   // define columns
-  {
-    id: {
+{
+  id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-  },
+},
   category_name: {
     type:DataTypes.STRING,
     allowNull:false,
-   },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+},
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 
 module.exports = Category;
