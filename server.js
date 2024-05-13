c3ee8f0fd2ac7bf3d280cb277339e14270e7f821
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-13 04:33:48 -0400

    Updated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./Develop/routes');
 
 // import sequelize connection
-const sequelize = require('./config/connection');
+const sequelize = require('./Develop/config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
