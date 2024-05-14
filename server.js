1a795f48a1be9ad9bc5e9e888752eeabe58b0a1e
Jaystarz360 <Jaystarz360@gmail.com>
2024-05-13 23:07:21 -0400

    Updated

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+// Set up requirements
 const express = require('express');
 const routes = require('./Develop/routes');
 
-// import sequelize connection
+// Import sequelize connection
 const sequelize = require('./Develop/config/connection');
 
+// Set up express and PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware JSON and urlencoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use
+// App.use
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync sequelize models to the database, then turn on the server
 sequelize.sync({force:false}).then(() =>{
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
